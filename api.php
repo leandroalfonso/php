9ef0a6a403a778d10bcb642dd3490f75c2437d3d
leandroalfonso <leandro.meira.alfonso@hotmail.com>
2023-07-16 08:18:28 -0300

    appl

--- api.php
+++ api.php
@@ -1,13 +1,12 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-
+header("Content-Type: application/json; charset=UTF-8");
 
 $dados = array(
     "nome" => "João da Silva",
     "idade" => 25,
     "cidade" => "São Paulo"
 );
-
 
 
 echo json_encode($dados);
